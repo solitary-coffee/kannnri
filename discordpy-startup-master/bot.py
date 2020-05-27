ea82b2bb927d822965a546b5309e93c0a2184022
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-05-27 20:38:21 +0900

    Add files via upload

---
+++ discordpy-startup-master/bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name='稼働中'))
+
+CHANNEL_ID = 709606780631777360
+	
+list = ["apple","orange","lemon","テスト"]
+ 
+@client.event
+async def on_message(message):    
+    for word in list:
+      if message.author.bot:
+       return
+      if word in message.content:
+        await message.delete()
+        await message.channel.send(message.author.mention +"\n" + '現在のコメントは削除されました\n理由：スパム判定になりました。\n身に覚えがない場合は孤独のコーヒーまで')
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send(message.author.mention+"\n" +" 削除されたコメント:" + word +"\n" +'コメントが削除されました\n誤作動の場合があります\n削除されたコメントを見ましょう\n誤作動の場合は削除されたコメントといっしょにコーヒーまで')
+
+
+
+
+
+
+
+           
+       
+
+client.run("NzEyNTk3MjE4MzgwNzQyNjk5.Xs5LnA.uFPoETQqeNH1bMk8UkK_pZLJ3zY")
