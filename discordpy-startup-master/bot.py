5956b704bf8ec79d3e626660dee01cc4a9d7b025
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-06-06 15:20:12 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -16,25 +16,26 @@ async def on_ready():
 
 CHANNEL_ID = 709606780631777360
 	
-
-
 @client.event
-async def on_message(message):    
+async def on_message(message):  
+    if message.content == '/list':
+      await message.channel.send(rog.list) 
     for word in rog.list:
       if message.author.bot:
         return
       if word in message.content:
         await message.delete()
         embed=discord.Embed(title="現在のコメントは削除されました",description=message.author.mention, color=0xdc0909)
-        embed.add_field(name="削除されたコメント:", value= word, inline=True)
+        embed.add_field(name="削除されたコメント(部分):", value= word, inline=True)
         embed.add_field(name="理由：", value="現在のコメントは暴言にあたります", inline=True)
-        embed.add_field(name="その他", value="意図しないで削除された場合は削除されたコメントと全文を孤独のコーヒーまでお願いします", inline=False)
-      
+        embed.add_field(name="削除されたコメント(全文):", value= message.content, inline=False)  
+        embed.add_field(name="違反したサーバー", value= message.guild, inline=True)
+        embed.add_field(name="違反したチャンネル", value= message.channel, inline=True)
+        embed.add_field(name="その他", value="意図しないで削除された場合は孤独のコーヒーまでお願いします", inline=False)
+        embed.add_field(name="違反した時間（UTC時間です日本時間は+９時間", value= message.created_at, inline=True)
+                                                      
 
-        await message.channel.send(message.author.mention)
-        await message.channel.send(embed=embed) 
-     
-
+        await message.channel.send(embed=embed)	
 
 
 
