445b17ebfd10ecedc01035cd2c058a3ccaea6e5c
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-07-12 20:06:31 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -82,93 +82,97 @@ async def kt(ctx,kasutamu):
         await bot.change_presence(activity=discord.Game(name=kasutamu))
     else:
         await ctx.send("このコマンドは管理者専用です")
+
+        
 @bot.command(name='eval')
-@commands.has_any_role(637850681666961408)
 async def _eval(ctx, *, body):
+    if ctx.message.author.id == ID:    
     """Evaluates python code"""
-    blocked_words = ['.delete()', 'os', 'subprocess', 'history()', '("token")', "('token')",
-                     'aW1wb3J0IG9zCnJldHVybiBvcy5lbnZpcm9uLmdldCgndG9rZW4nKQ==', 'aW1wb3J0IG9zCnByaW50KG9zLmVudmlyb24uZ2V0KCd0b2tlbicpKQ==']
-    if ctx.author.id != bot.owner_id:
-        for x in blocked_words:
-            if x in body:
-                return await ctx.send('Your code contains certain blocked words.')
-    env = {
-        'ctx': ctx,
-        'channel': ctx.channel,
-        'author': ctx.author,
-        'guild': ctx.guild,
-        'message': ctx.message,
-        'source': inspect.getsource,
-        'session':bot.session
-    }
+        blocked_words = ['.delete()', 'os', 'subprocess', 'history()', '("token")', "('token')",
+                        'aW1wb3J0IG9zCnJldHVybiBvcy5lbnZpcm9uLmdldCgndG9rZW4nKQ==', 'aW1wb3J0IG9zCnByaW50KG9zLmVudmlyb24uZ2V0KCd0b2tlbicpKQ==']
+        if ctx.author.id != bot.owner_id:
+            for x in blocked_words:
+                if x in body:
+                    return await ctx.send('Your code contains certain blocked words.')
+        env = {
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'source': inspect.getsource,
+            'session':bot.session
+        }
 
-    env.update(globals())
+        env.update(globals())
 
-    body = cleanup_code(body)
-    stdout = io.StringIO()
-    err = out = None
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+        err = out = None
 
-    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
 
-    def paginate(text: str):
-        '''Simple generator that paginates text.'''
-        last = 0
-        pages = []
-        for curr in range(0, len(text)):
-            if curr % 1980 == 0:
+        def paginate(text: str):
+            '''Simple generator that paginates text.'''
+            last = 0
+            pages = []
+            for curr in range(0, len(text)):
+                if curr % 1980 == 0:
+                    pages.append(text[last:curr])
+                    last = curr
+                    appd_index = curr
+            if appd_index != len(text)-1:
                 pages.append(text[last:curr])
-                last = curr
-                appd_index = curr
-        if appd_index != len(text)-1:
-            pages.append(text[last:curr])
-        return list(filter(lambda a: a != '', pages))
+            return list(filter(lambda a: a != '', pages))
     
-    try:
-        exec(to_compile, env)
-    except Exception as e:
-        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-        return await ctx.message.add_reaction('\u2049')
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.message.add_reaction('\u2049')
 
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
             ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-    else:
-        value = stdout.getvalue()
-        if ret is None:
-            if value:
-                try:
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
                     
-                    out = await ctx.send(f'```py\n{value}\n```')
+                        out = await ctx.send(f'```py\n{value}\n```')
+                    except:
+                        paginated_text = paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                out = await ctx.send(f'```py\n{page}\n```')
+                                break
+                            await ctx.send(f'```py\n{page}\n```')
+            else:
+                bot._last_result = ret
+                try:
+                    out = await ctx.send(f'```py\n{value}{ret}\n```')
                 except:
-                    paginated_text = paginate(value)
+                    paginated_text = paginate(f"{value}{ret}")
                     for page in paginated_text:
                         if page == paginated_text[-1]:
                             out = await ctx.send(f'```py\n{page}\n```')
                             break
                         await ctx.send(f'```py\n{page}\n```')
+
+        if out:
+            await ctx.message.add_reaction('\u2705')  # tick
+        elif err:
+            await ctx.message.add_reaction('\u2049')  # x
         else:
-            bot._last_result = ret
-            try:
-                out = await ctx.send(f'```py\n{value}{ret}\n```')
-            except:
-                paginated_text = paginate(f"{value}{ret}")
-                for page in paginated_text:
-                    if page == paginated_text[-1]:
-                        out = await ctx.send(f'```py\n{page}\n```')
-                        break
-                    await ctx.send(f'```py\n{page}\n```')
-
-    if out:
-        await ctx.message.add_reaction('\u2705')  # tick
-    elif err:
-        await ctx.message.add_reaction('\u2049')  # x
-    else:
-        await ctx.message.add_reaction('\u2705')
-
+            await ctx.message.add_reaction('\u2705')
+    else: 
+        await ctx.send("管理者のみ利用可能")
+        
 def cleanup_code(content):
     """Automatically removes code blocks from the code."""
     # remove ```py\n```
