43e4e9a7e9ef8452cccc1208f7e0e5f01967b3bd
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-07-13 19:22:13 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -100,7 +100,7 @@ async def _eval(ctx, *, body):
             'guild': ctx.guild,
             'message': ctx.message,
             'source': inspect.getsource,
-            'session':ctx.session
+            'session':clientsession
         }
 
         env.update(globals())
