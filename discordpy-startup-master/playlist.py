1d2cefc0f5b47d68deb77959f9bfe2d00a2a355a
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-10 18:37:19 +0900

    Create playlist.py

---
+++ discordpy-startup-master/playlist.py
@@ -0,0 +1,6 @@
+playlist = ["https://www.youtube.com/watch?v=_IH5d6IByfw",
+            "https://www.youtube.com/watch?v=M1U56TwwUgw",
+            "https://www.youtube.com/watch?v=6JCsjw6OkpU",
+            "https://www.youtube.com/watch?v=zWQ1kpRmc5Y",
+
+]
