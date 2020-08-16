fc32ae684dd08811fe23bb0cd9d731193870c993
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-16 14:31:49 +0900

    Update playlist.py

--- discordpy-startup-master/playlist.py
+++ discordpy-startup-master/playlist.py
@@ -2,5 +2,9 @@ playlist = ["https://www.youtube.com/watch?v=_IH5d6IByfw",
             "https://www.youtube.com/watch?v=M1U56TwwUgw",
             "https://www.youtube.com/watch?v=6JCsjw6OkpU",
             "https://www.youtube.com/watch?v=zWQ1kpRmc5Y",
+            "https://www.youtube.com/watch?v=R6E21MFhmBQ",
+            "https://www.youtube.com/watch?v=T0NH1xNV7RM",
+            "https://www.youtube.com/watch?v=HR9Wkf_euf4",
+            
 
 ]
