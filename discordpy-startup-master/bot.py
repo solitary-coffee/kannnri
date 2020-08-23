44837fef931cfcb9d38c05aa6aef35f83dcd1ff9
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-23 10:00:25 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -94,7 +94,47 @@ async def on_message(message):
     elif message.channel.id == 713588195555541033:
         print("ok")
     elif message.channel.id == 721837476666409010:
+       
         print("ok")
+        
+    if message.author.bot:
+        return
+#原型
+
+    elif message.content.startswith('!trans'):
+        say = message.content
+        say = say[7:]
+        if say.find('-') == -1:
+            str = say
+            detact = translator.detect(str)
+            befor_lang = detact.lang
+            if befor_lang == 'ja':
+                convert_string = translator.translate(str, src=befor_lang, dest='en')
+                embed = discord.Embed(title='変換結果', color=0xff0000)
+                embed.add_field(name='Befor', value=str)
+                embed.add_field(name='After', value=convert_string.text, inline=False)
+                await message.channel.send(embed=embed)
+            else:
+                convert_string = translator.translate(str, src=befor_lang, dest='ja')
+                embed = discord.Embed(title='変換結果', color=0xff0000)
+                embed.add_field(name='Befor', value=str)
+                embed.add_field(name='After', value=convert_string.text, inline=False)
+                await message.channel.send(embed=embed)
+        else:
+            trans, str = list(say.split('='))
+            befor_lang, after_lang = list(trans.split('-'))
+            convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
+            embed = discord.Embed(title='変換結果', color=0xff0000)
+            embed.add_field(name='Befor', value=str)
+            embed.add_field(name='After', value=convert_string.text, inline=False)
+            await message.channel.send(embed=embed)
+
+    elif message.content.startswith('!detect'):
+        say = message.content
+        s = say[8:]
+        detect = translator.detect(s)
+        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
+        await message.channel.send(m)
     else:       
         for word in rog.list:
             if message.author.bot:
