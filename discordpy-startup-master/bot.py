71056d21ee615c367b79a1712764bb5378eda727
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-26 20:36:59 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -90,11 +90,33 @@ async def dm_error(ctx, error):
 
 
 @bot.event
-async def on_message(message):      
+async def on_message(message):          
+    if message.guild.id != 727850593330397265:      
+        for word in rog.list:
+            if message.author.bot:
+                return
+            if word in message.content:
+                await message.delete()
+                embed=discord.Embed(title="現在のコメントは削除されました",description=message.author.mention, color=0xdc0909)
+                embed.add_field(name="削除されたコメント(部分):", value= word, inline=True)
+                embed.add_field(name="理由：", value="現在のコメントは暴言にあたります", inline=True)
+                embed.add_field(name="削除されたコメント(全文):", value= message.content, inline=False)  
+                embed.add_field(name="違反したサーバー", value= message.guild, inline=True)
+                embed.add_field(name="違反したチャンネル", value= message.channel, inline=True)
+                embed.add_field(name="その他", value="意図しないで削除された場合は孤独のコーヒーまでお願いします", inline=False)
+                embed.add_field(name="違反した時間（UTC時間です日本時間は+９時間", value= message.created_at, inline=True)
+                                                      
 
-    GLOBAL_CH_NAME = "am-go" # グローバルチャットのチャンネル名
+                await message.channel.send(embed=embed)
+    await bot.process_commands(message)
+    
+@client.event
+async def on_message(message):
     if message.author.bot:
+        # もし、送信者がbotなら無視する
         return
+    GLOBAL_CH_NAME = "am-go" # グローバルチャットのチャンネル名
+
     if message.channel.name == GLOBAL_CH_NAME:
         # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
 
@@ -117,25 +139,6 @@ async def on_message(message):
         for channel in global_channels:
             # メッセージを埋め込み形式で転送
             await channel.send(embed=embed)
-    
-    elif message.guild.id != 727850593330397265:      
-        for word in rog.list:
-            if message.author.bot:
-                return
-            if word in message.content:
-                await message.delete()
-                embed=discord.Embed(title="現在のコメントは削除されました",description=message.author.mention, color=0xdc0909)
-                embed.add_field(name="削除されたコメント(部分):", value= word, inline=True)
-                embed.add_field(name="理由：", value="現在のコメントは暴言にあたります", inline=True)
-                embed.add_field(name="削除されたコメント(全文):", value= message.content, inline=False)  
-                embed.add_field(name="違反したサーバー", value= message.guild, inline=True)
-                embed.add_field(name="違反したチャンネル", value= message.channel, inline=True)
-                embed.add_field(name="その他", value="意図しないで削除された場合は孤独のコーヒーまでお願いします", inline=False)
-                embed.add_field(name="違反した時間（UTC時間です日本時間は+９時間", value= message.created_at, inline=True)
-                                                      
-
-                await message.channel.send(embed=embed)
-    await bot.process_commands(message)
 @bot.command()
 async def kt(ctx, *,kasutamu):
     if ctx.message.author.id == ID:    
