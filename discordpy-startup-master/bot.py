eee49c0c5629fc5b830fd40d25fed254ee174263
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-26 19:47:03 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -92,6 +92,31 @@ async def dm_error(ctx, error):
 async def on_message(message):      
     if message.guild.id== 727850593330397265:
         print("ok")
+    GLOBAL_CH_NAME = "am-go" # グローバルチャットのチャンネル名
+
+    elif message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+
+        await message.delete() # 元のメッセージは削除しておく
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+        # channelsはbotの取得できるチャンネルのイテレーター
+        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+
+        embed = discord.Embed(title="```↑送信者　↓　内容```",
+            description=message.content, color=0x00bfff)
+
+        embed.set_author(name=message.author.display_name, 
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))
+        # Embedインスタンスを生成、投稿者、投稿場所などの設定
+
+        for channel in global_channels:
+            # メッセージを埋め込み形式で転送
+            await channel.send(embed=embed)
+
     else:               
         for word in rog.list:
             if message.author.bot:
