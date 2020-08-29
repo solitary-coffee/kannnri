be34753383b3a8d596036ce4de2516285b2ecec2
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-29 18:20:57 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -118,6 +118,40 @@ async def on_message(message):
         for channel in global_channels:
             # メッセージを埋め込み形式で転送
             await channel.send(embed=embed)
+    if message.content.startswith('!trans'):
+        say = message.content
+        say = say[7:]
+        if say.find('-') == -1:
+            str = say
+            detact = translator.detect(str)
+            befor_lang = detact.lang
+            if befor_lang == 'ja':
+                convert_string = translator.translate(str, src=befor_lang, dest='en')
+                embed = discord.Embed(title='変換結果', color=0xff0000)
+                embed.add_field(name='Befor', value=str)
+                embed.add_field(name='After', value=convert_string.text, inline=False)
+                await message.channel.send(embed=embed)
+            else:
+                convert_string = translator.translate(str, src=befor_lang, dest='ja')
+                embed = discord.Embed(title='変換結果', color=0xff0000)
+                embed.add_field(name='Befor', value=str)
+                embed.add_field(name='After', value=convert_string.text, inline=False)
+                await message.channel.send(embed=embed)
+        else:
+            trans, str = list(say.split('='))
+            befor_lang, after_lang = list(trans.split('-'))
+            convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
+            embed = discord.Embed(title='変換結果', color=0xff0000)
+            embed.add_field(name='Befor', value=str)
+            embed.add_field(name='After', value=convert_string.text, inline=False)
+            await message.channel.send(embed=embed)
+
+    if message.content.startswith('!detect'):
+        say = message.content
+        s = say[8:]
+        detect = translator.detect(s)
+        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
+        await message.channel.send(m)
 
     if message.guild.id != 727850593330397265:      
         for word in rog.list:
