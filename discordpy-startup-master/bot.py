fd1fc330b8babd480e0586d02716221402e388ed
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-08-29 18:27:51 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -153,23 +153,7 @@ async def on_message(message):
         m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
         await message.channel.send(m)
 
-    if message.guild.id != 727850593330397265:      
-        for word in rog.list:
-            if message.author.bot:
-                return
-            if word in message.content:
-                await message.delete()
-                embed=discord.Embed(title="現在のコメントは削除されました",description=message.author.mention, color=0xdc0909)
-                embed.add_field(name="削除されたコメント(部分):", value= word, inline=True)
-                embed.add_field(name="理由：", value="現在のコメントは暴言にあたります", inline=True)
-                embed.add_field(name="削除されたコメント(全文):", value= message.content, inline=False)  
-                embed.add_field(name="違反したサーバー", value= message.guild, inline=True)
-                embed.add_field(name="違反したチャンネル", value= message.channel, inline=True)
-                embed.add_field(name="その他", value="意図しないで削除された場合は孤独のコーヒーまでお願いします", inline=False)
-                embed.add_field(name="違反した時間（UTC時間です日本時間は+９時間", value= message.created_at, inline=True)
-                                                      
 
-                await message.channel.send(embed=embed)
     await bot.process_commands(message)
     
 
