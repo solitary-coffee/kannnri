a88f47287e33bbe0dc20751ac907c2ab1689ca73
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-09-10 20:28:44 +0900

    Update bot.py

--- discordpy-startup-master/bot.py
+++ discordpy-startup-master/bot.py
@@ -110,26 +110,29 @@ async def on_message(message):
 
     if message.channel.name == GLOBAL_CH_NAME:
         # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+            if message.author.id in glist:
+                await message.channel.send("あなたはＧＢＡＮされています")
+                return
+            else:
+                await message.delete() # 元のメッセージは削除しておく
 
-        await message.delete() # 元のメッセージは削除しておく
+                channels = client.get_all_channels()
+                global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+                # channelsはbotの取得できるチャンネルのイテレーター
+                # global_channelsは hoge-global の名前を持つチャンネルのリスト
 
-        channels = bot.get_all_channels()
-        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
-        # channelsはbotの取得できるチャンネルのイテレーター
-        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+                embed = discord.Embed(title="```↑送信者　↓　内容```",
+                    description=message.content, color=0x00bfff)
 
-        embed = discord.Embed(title=f"ID:{message.author.id}",
-            description=message.content, color=0x00bfff)
+                embed.set_author(name=message.author.display_name, 
+                    icon_url=message.author.avatar_url_as(format="png"))
+                embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+                    icon_url=message.guild.icon_url_as(format="png"))
+                # Embedインスタンスを生成、投稿者、投稿場所などの設定
 
-        embed.set_author(name=message.author.display_name, 
-            icon_url=message.author.avatar_url_as(format="png"))
-        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
-            icon_url=message.guild.icon_url_as(format="png"))
-        # Embedインスタンスを生成、投稿者、投稿場所などの設定
-
-        for channel in global_channels:
-            # メッセージを埋め込み形式で転送
-            await channel.send(embed=embed)
+                for channel in global_channels:
+                # メッセージを埋め込み形式で転送
+                    await channel.send(embed=embed)
     if message.content.startswith('!trans'):
         say = message.content
         say = say[7:]
