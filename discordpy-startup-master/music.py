fbe4381c0be06e851b590945914cb03269eeb54b
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-09-24 20:22:23 +0900

    Add files via upload

---
+++ discordpy-startup-master/music.py
@@ -0,0 +1,316 @@
+import math
+
+import discord
+from discord.ext import commands
+
+import ytdl
+import voice
+
+class Music(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.voice_states = {}
+
+    def get_voice_state(self, ctx: commands.Context):
+        """Returns or creates voice.VoiceState for the guild defined in the passed ctx"""
+        state = self.voice_states.get(ctx.guild.id)
+        if not state or not state.exists:
+            state = voice.VoiceState(self.bot, ctx)
+            self.voice_states[ctx.guild.id] = state
+
+        return state
+
+    def cog_unload(self):
+        """Unloads the music cog"""
+        for state in self.voice_states.values():
+            self.bot.loop.create_task(state.stop())
+
+    def cog_check(self, ctx: commands.Context):
+        """Prevent calling commands in DM's"""
+        if not ctx.guild:
+            raise commands.NoPrivateMessage('DMでは使えません ')
+
+        return True
+
+    async def cog_before_invoke(self, ctx: commands.Context):
+        #Set voice state for every command
+        ctx.voice_state = self.get_voice_state(ctx)
+
+    async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        await ctx.send('エラー: {}'.format(str(error)))
+
+
+
+    @commands.command(name='join', invoke_without_subcommand=True)
+    async def _join(self, ctx: commands.Context):
+        """Joins a voice channel."""
+
+        destination = ctx.author.voice.channel
+        if ctx.voice_state.voice:
+            await ctx.voice_state.voice.move_to(destination)
+            return
+
+        ctx.voice_state.voice = await destination.connect()
+
+    @commands.command(name='summon')
+    @commands.has_permissions(manage_guild=True)
+    async def _summon(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None):
+        """Summons the bot to a voice channel.
+        If no channel was specified, it joins your channel.
+        """
+
+        if not channel and not ctx.author.voice:
+            raise voice.VoiceError('あなたは音声チャンネルに接続していないか、参加するチャンネルを指定していません')
+
+        destination = channel or ctx.author.voice.channel
+        if ctx.voice_state.voice:
+            await ctx.voice_state.voice.move_to(destination)
+            return
+
+        ctx.voice_state.voice = await destination.connect()
+
+    @commands.command(name='leave', aliases=['disconnect'])
+    @commands.has_permissions(manage_guild=True)
+    async def _leave(self, ctx: commands.Context):
+        """Clears the queue and leaves the voice channel."""
+
+        if not ctx.voice_state.voice:
+            return await ctx.send('どの音声チャネルにも接続されていません')
+
+        await ctx.voice_state.stop()
+        del self.voice_states[ctx.guild.id]
+
+    @commands.command(name='vol')
+    @commands.is_owner()
+    async def _volume(self, ctx: commands.Context, *, volume: int):
+        """Sets the volume of the player."""
+
+        if not ctx.voice_state.is_playing:
+            return await ctx.send('何も再生されていません.')
+
+        if 0 > volume > 100:
+            return await ctx.send('ボリュームは0から100の間でなければなりません')
+
+        ctx.voice_state.volume = volume / 100
+        await ctx.send('ボリューム設定：{}%'.format(volume))
+
+    @commands.command(name='now', aliases=['current', 'playing', 'np', 'nowplaying'])
+    async def _now(self, ctx: commands.Context):
+        """Displays the currently playing song."""
+        embed = ctx.voice_state.current.create_embed()
+        await ctx.send(embed=embed)
+
+    @commands.command(name='pause', aliases=['pa'])
+    @commands.has_permissions(manage_guild=True)
+    async def _pause(self, ctx: commands.Context):
+        """Pauses the currently playing song."""
+        print(">>>Pause Command:")
+        if ctx.voice_state.is_playing and ctx.voice_state.voice.is_playing():
+            ctx.voice_state.voice.pause()
+            await ctx.message.add_reaction('⏯')
+
+    @commands.command(name='resume', aliases=['re', 'res'])
+    @commands.has_permissions(manage_guild=True)
+    async def _resume(self, ctx: commands.Context):
+        """Resumes a currently paused song."""
+
+        if ctx.voice_state.is_playing and ctx.voice_state.voice.is_paused():
+            ctx.voice_state.voice.resume()
+            await ctx.message.add_reaction('⏯')
+
+    @commands.command(name='stop')
+    @commands.has_permissions(manage_guild=True)
+    async def _stop(self, ctx: commands.Context):
+        """Stops playing song and clears the queue."""
+
+        ctx.voice_state.songs.clear()
+
+        if ctx.voice_state.autoplay:
+            ctx.voice_state.autoplay = False
+            await ctx.send('自動再生がオフになりました')
+            
+        if ctx.voice_state.is_playing:
+            ctx.voice_state.voice.stop()
+            await ctx.message.add_reaction('⏹')
+
+
+    @commands.command(name='skip', aliases=['s'])
+    async def _skip(self, ctx: commands.Context):
+        """Vote to skip a song. The requester can automatically skip.
+        3 skip votes are needed for the song to be skipped.
+        """
+
+        if not ctx.voice_state.is_playing:
+            return await ctx.send('音楽を再生していません')
+
+        voter = ctx.message.author
+        if voter == ctx.voice_state.current.requester:
+            await ctx.message.add_reaction('⏭')
+            ctx.voice_state.skip()
+
+        elif voter.id not in ctx.voice_state.skip_votes:
+            ctx.voice_state.skip_votes.add(voter.id)
+            total_votes = len(ctx.voice_state.skip_votes)
+
+            if total_votes >= 3:
+                await ctx.message.add_reaction('⏭')
+                ctx.voice_state.skip()
+            else:
+                await ctx.send('スキップするには投票が必要です **{}/3**'.format(total_votes))
+
+        else:
+            await ctx.send('この曲をスキップするようにすでに投票しています')
+
+    @commands.command(name='queue')
+    async def _queue(self, ctx: commands.Context, *, page: int = 1):
+        """Shows the player's queue.
+        You can optionally specify the page to show. Each page contains 10 elements.
+        """
+
+        if len(ctx.voice_state.songs) == 0:
+            return await ctx.send('待機リストにはなにも入っていません')
+
+        items_per_page = 10
+        pages = math.ceil(len(ctx.voice_state.songs) / items_per_page)
+
+        start = (page - 1) * items_per_page
+        end = start + items_per_page
+
+        queue = ''
+        for i, song in enumerate(ctx.voice_state.songs[start:end], start=start):
+            queue += '`{0}.` [**{1.source.title}**]({1.source.url})\n'.format(i + 1, song)
+
+        embed = (discord.Embed(description='**{} 曲:**\n\n{}'.format(len(ctx.voice_state.songs), queue))
+                 .set_footer(text='リストページ {}/{}'.format(page, pages)))
+        await ctx.send(embed=embed)
+
+    @commands.command(name='history')
+    async def _history(self, ctx: commands.Context, *, page: int = 1):
+        """Shows the player's history.
+        You can optionally specify the page to show. Each page contains 10 elements.
+        """
+
+        if len(ctx.voice_state.song_history) == 0:
+            return await ctx.send('なにも登録されていません')
+
+        items_per_page = 10
+        pages = math.ceil(len(ctx.voice_state.song_history) / items_per_page)
+
+        start = (page - 1) * items_per_page
+        end = start + items_per_page
+
+        queue = ''
+        for i, song in enumerate(ctx.voice_state.song_history[start:end], start=start):
+            queue += '`{0}.` [**{1.source.title}**]({1.source.url})\n'.format(i + 1, song)
+
+        embed = (discord.Embed(description='**{} 曲:**\n\n{}'.format(len(ctx.voice_state.song_history), queue))
+                 .set_footer(text='リストページ {}/{}'.format(page, pages)))
+        await ctx.send(embed=embed)
+
+    @commands.command(name='mix')
+    async def _shuffle(self, ctx: commands.Context):
+        """Shuffles the queue."""
+
+        if len(ctx.voice_state.songs) == 0:
+            return await ctx.send('待機リストにはなにも入っていません.')
+
+        ctx.voice_state.songs.shuffle()
+        await ctx.message.add_reaction('✅')
+
+    @commands.command(name='remove')
+    async def _remove(self, ctx: commands.Context, index: int):
+        """Removes a song from the queue at a given index."""
+
+        if len(ctx.voice_state.songs) == 0:
+            return await ctx.send('待機リストにはなにも入っていません')
+
+        ctx.voice_state.songs.remove(index - 1)
+        await ctx.message.add_reaction('✅')
+
+    @commands.command(name='loop')
+    async def _loop(self, ctx: commands.Context):
+        """Loops the currently playing song.
+        Invoke this command again to unloop the song.
+        """
+
+        if not ctx.voice_state.is_playing:
+            return await ctx.send('何も再生されていません')
+
+        # Inverse boolean value to loop and unloop.
+        ctx.voice_state.loop = not ctx.voice_state.loop
+        await ctx.message.add_reaction('✅')
+        await ctx.send('ループ機能' + ('on' if ctx.voice_state.loop else 'off') + 'にしました' )
+
+    @commands.command(name='autoplay')
+    async def _autoplay(self, ctx: commands.Context):
+        """Automatically queue a new song that is related to the song at the end of the queue.
+        Invoke this command again to toggle autoplay the song.
+        """
+
+        if not ctx.voice_state.is_playing:
+            return await ctx.send('何も再生されていません')
+
+        # Inverse boolean value to loop and unloop.
+        ctx.voice_state.autoplay = not ctx.voice_state.autoplay
+        await ctx.message.add_reaction('✅')
+        await ctx.send('オート再生機能' + ('on' if ctx.voice_state.autoplay else 'off') + 'にしました' )
+
+    @commands.command(name='play', aliases=['p'])
+    async def _play(self, ctx: commands.Context, *, search: str):
+        """Plays a song.
+        If there are songs in the queue, this will be queued until the
+        other songs finished playing.
+        This command automatically searches from various sites if no URL is provided.
+        A list of these sites can be found here: https://rg3.github.io/youtube-dl/supportedsites.html
+        """
+
+        async with ctx.typing():
+            try:
+                source = await ytdl.YTDLSource.create_source(ctx, search, loop=self.bot.loop)
+            except ytdl.YTDLError as e:
+                await ctx.send('エラー: {}'.format(str(e)))
+            else:
+                if not ctx.voice_state.voice:
+                    await ctx.invoke(self._join)
+
+                song = voice.Song(source)
+                await ctx.voice_state.songs.put(song)
+                await ctx.send('リストに追加 {}'.format(str(source)))
+
+    @commands.command(name='se')
+    async def _search(self, ctx: commands.Context, *, search: str):
+        """Searches youtube.
+        It returns an imbed of the first 10 results collected from youtube.
+        Then the user can choose one of the titles by typing a number
+        in chat or they can cancel by typing "cancel" in chat.
+        Each title in the list can be clicked as a link.
+        """
+        async with ctx.typing():
+            try:
+                source = await ytdl.YTDLSource.search_source(self.bot, ctx, search, loop=self.bot.loop)
+            except ytdl.YTDLError as e:
+                await ctx.send('エラー: {}'.format(str(e)))
+            else:
+                if source == 'sel_invalid':
+                    await ctx.send('Invalid selection')
+                elif source == 'cancel':
+                    await ctx.send(':white_check_mark:')
+                elif source == 'timeout':
+                    await ctx.send(':alarm_clock: **待機時間をがすぎました**')
+                else:
+                    if not ctx.voice_state.voice:
+                        await ctx.invoke(self._join)
+
+                    song = voice.Song(source)
+                    await ctx.voice_state.songs.put(song)
+                    await ctx.send('リストに追加 {}'.format(str(source)))
+            
+    @_join.before_invoke
+    @_play.before_invoke
+    async def ensure_voice_state(self, ctx: commands.Context):
+        if not ctx.author.voice or not ctx.author.voice.channel:
+            raise commands.CommandError('音声チャネルに接続されていません')
+
+        if ctx.voice_client:
+            if ctx.voice_client.channel != ctx.author.voice.channel:
+                raise commands.CommandError('ボットはすでに音声チャネルにあります')
