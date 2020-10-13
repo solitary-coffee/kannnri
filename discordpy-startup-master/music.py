b7422c75a92cb16d564778c9361a081d11d74365
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-10-13 15:31:13 +0900

    Update music.py

--- discordpy-startup-master/music.py
+++ discordpy-startup-master/music.py
@@ -71,8 +71,7 @@ class Music(commands.Cog):
 
         ctx.voice_state.voice = await destination.connect()
 
-    @commands.command(name='leave', aliases=['disconnect'])
-    @commands.has_permissions(manage_guild=True)
+    @commands.command(name='leave', aliases=['disconnect']
     async def _leave(self, ctx: commands.Context):
         """Clears the queue and leaves the voice channel."""
 
