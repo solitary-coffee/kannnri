d9386584e7171f49be645a1149aef0eb0a7a190c
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-10-14 19:07:01 +0900

    Update main.py

--- discordpy-startup-master/main.py
+++ discordpy-startup-master/main.py
@@ -24,65 +24,7 @@ async def on_ready():
         await asyncio.sleep(5)
         await bot.change_presence(activity=discord.Game(name="グローバルチャンネル名をcoffee-global"))
         await asyncio.sleep(15)
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        # もし、送信者がbotなら無視する
-        return
-    if message.content.startswith('!trans'):
-        say = message.content
-        say = say[7:]
-        if say.find('-') == -1:
-            str = say
-            detact = translator.detect(str)
-            befor_lang = detact.lang
-            if befor_lang == 'ja':
-                convert_string = translator.translate(str, src=befor_lang, dest='en')
-                embed = discord.Embed(title='変換結果', color=0xff0000)
-                embed.add_field(name='Befor', value=str)
-                embed.add_field(name='After', value=convert_string.text, inline=False)
-                await message.channel.send(embed=embed)
-            else:
-                convert_string = translator.translate(str, src=befor_lang, dest='ja')
-                embed = discord.Embed(title='変換結果', color=0xff0000)
-                embed.add_field(name='Befor', value=str)
-                embed.add_field(name='After', value=convert_string.text, inline=False)
-                await message.channel.send(embed=embed)
-        else:
-            trans, str = list(say.split('='))
-            befor_lang, after_lang = list(trans.split('-'))
-            convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
-            embed = discord.Embed(title='変換結果', color=0xff0000)
-            embed.add_field(name='Befor', value=str)
-            embed.add_field(name='After', value=convert_string.text, inline=False)
-            await message.channel.send(embed=embed)
 
-    if message.content.startswith('!detect'):
-        say = message.content
-        s = say[8:]
-        detect = translator.detect(s)
-        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
-        await message.channel.send(m)
-    if message.guild.id not in buno.gila:      
-        for word in rog.list:
-            if message.author.bot:
-                return
-            if word in message.content:
-                await message.delete()
-                embed=discord.Embed(title="現在のコメントは削除されました",description=message.author.mention, color=0xdc0909)
-                embed.add_field(name="削除されたコメント(部分):", value= word, inline=True)
-                embed.add_field(name="理由：", value="現在のコメントは暴言にあたります", inline=True)
-                embed.add_field(name="削除されたコメント(全文):", value= message.content, inline=False)  
-                embed.add_field(name="違反したサーバー", value= message.guild, inline=True)
-                embed.add_field(name="違反したチャンネル", value= message.channel, inline=True)
-                embed.add_field(name="その他", value="意図しないで削除された場合は孤独のコーヒーまでお願いします", inline=False)
-                embed.add_field(name="違反した時間（UTC時間です日本時間は+９時間", value= message.created_at, inline=True)
-                                                      
-
-                await message.channel.send(embed=embed)  
-
-
-    await bot.process_commands(message)
 
 bot.load_extension("model.sub")
 bot.load_extension("model.dm")
