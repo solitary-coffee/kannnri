2bee1ba6b6f659901ba49337e07123baa0240233
jr226-co <64074181+jr226-co@users.noreply.github.com>
2020-10-15 19:44:36 +0900

    Update playlist.py
    llll

--- discordpy-startup-master/deta/playlist.py
+++ discordpy-startup-master/deta/playlist.py
@@ -9,38 +9,5 @@ sui  =[
             "https://www.youtube.com/watch?v=HR9Wkf_euf4"
             
 
-]
-
-bokaro  =[ 
-            "https://www.youtube.com/watch?v=qnX2CdOBcDI&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9",
-            "https://www.youtube.com/watch?v=AS4q9yaWJkI&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=2",
-            "https://www.youtube.com/watch?v=HOz-9FzIDf0&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=3",
-            "https://www.youtube.com/watch?v=0RU_05zpETo&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=4",
-            "https://www.youtube.com/watch?v=Ut64ptd0bYk&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=5",
-            "https://www.youtube.com/watch?v=hxSg2Ioz3LM&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=6",
-            "https://www.youtube.com/watch?v=KushW6zvazM&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=7",
-            "https://www.youtube.com/watch?v=Om3MTou2kPg&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=11",
-            "https://www.youtube.com/watch?v=MqNmKnCNLyM&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=12",
-            "https://www.youtube.com/watch?v=Ey_NHZNYTeE&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=13",
-            "https://www.youtube.com/watch?v=b2GJcYBoVyg&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=14",
-            "https://www.youtube.com/watch?v=Xg-qfsKN2_E&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=15",
-            "https://www.youtube.com/watch?v=xxFkW3PCT5M&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=16",
-            "https://www.youtube.com/watch?v=XogSflwXgpw&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=22",
-            "https://www.youtube.com/watch?v=hZFBTnzKa54&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=23",
-            "https://www.youtube.com/watch?v=0HYm60Mjm0k",
-            "https://www.youtube.com/watch?v=EMGyiiTC7sg&list=PLzWMkQxtSEzKto-Gey8TPJU4ppJ0hbkM9&index=30"
-            
-
-]
-
-perc  =[ 
-            "https://www.youtube.com/watch?v=NNFVnmrSFC0&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA",
-            "https://www.youtube.com/watch?v=6joEIyaoOEM&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA&index=2",
-            "https://www.youtube.com/watch?v=m5FwjWF0PpE&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA&index=3",
-            "https://www.youtube.com/watch?v=yofI1V2WKEI&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA&index=4",
-            "https://www.youtube.com/watch?v=JWv3B1XQ3m4&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA&index=5",
-            "https://www.youtube.com/watch?v=ZWaeEujAzJg&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA&index=6",
-            "https://www.youtube.com/watch?v=iv2eHJ7f-Qg&list=PLzWMkQxtSEzK2IjDauzye56iFuXpoZtJA&index=7"
-            
 
 ]
